0b14ace807d481866096029d559111fb59981f32
AbdullahKG <abdullah.aboode.omar@gmail.com>
2024-11-30 15:31:46 +0300

    to emit cardid to the UI

---
+++ socketio/socket.js
@@ -0,0 +1,36 @@
+const { Server } = require('socket.io');
+
+let io;
+
+const initializeSocket = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log(`Device connected: ${socket.id}`);
+
+    // Handle device disconnection
+    socket.on('disconnect', () => {
+      console.log(`Device disconnected: ${socket.id}`);
+    });
+  });
+};
+
+// Function to send Card ID to a specific device
+const sendCardIDToDevice = (cardID) => {
+  if (cardID) {
+    io.emit('cardID', cardID);
+    console.log(`Card ID : ${cardID} sent successfuly`);
+  } else {
+    console.error(`error while sending`);
+  }
+};
+
+module.exports = {
+  initializeSocket,
+  sendCardIDToDevice,
+};
