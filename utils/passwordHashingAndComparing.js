e30ecbdca5585df81b3680b1400d179c2f4e3a8e
AbdullahKG <abdullah.aboode.omar@gmail.com>
2024-12-12 04:04:32 +0300

    removed async/await

--- utils/passwordHashingAndComparing.js
+++ utils/passwordHashingAndComparing.js
@@ -1,10 +1,9 @@
 const bcrypt = require('bcryptjs');
-const catchAsync = require('./catchAsync');
 
-const hashPasword = catchAsync(async (password) => {
-  const hashedPassword = await bcrypt.hash(password, 12);
+const hashPasword = (password) => {
+  const hashedPassword = bcrypt.hash(password, 12);
   return hashedPassword;
-});
+};
 
 const comparePassword = (candidatePassword, hashedPassword) => {
   return bcrypt.compare(candidatePassword, hashedPassword);
